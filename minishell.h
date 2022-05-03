5ef44aed4b2e7e02fd9b4153a534b12f7fbe1d96
Pablo Villena <pvillena@student.42madrid.com>
2022-05-03 14:11:32 +0200

    readline started

--- minishell.h
+++ minishell.h
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvillena <pvillena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 14:10:38 by pvillena          #+#    #+#             */
-/*   Updated: 2022/05/03 14:10:48 by pvillena         ###   ########.fr       */
+/*   Created: 2022/05/03 14:09:47 by pvillena          #+#    #+#             */
+/*   Updated: 2022/05/03 14:10:28 by pvillena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-void	leaks(void)
-{
-	system("leaks minishell");
-}
-
-int	main(void)
-{
-	char	*read;
-
-	atexit(leaks);
-	while (1)
-	{
-		read = readline("minishell> ");
-		if(*read)
-			add_history(read);
-		if (ft_strncmp(read, "exit", 100) == 0)
-			exit(0);
-		free(read);
-	}
-	return (0);
-}
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+#endif
